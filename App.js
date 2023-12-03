4944f1d83979beb7e942b3dddb842956200ae609
shivasakteballan <91557200+shivasakteballan@users.noreply.github.com>
2023-12-03 23:13:53 +0800

    Updated

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
+import { StatusBar, Button } from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-// import Dashboard from './src/screens/Dashboard';
-import LoginScreen from './src/screens/Login';
+import Dashboard from './src/screens/Dashboard';
+import LoginScreen from './src/screens/Login/Login';
+import TransactionHistory from './src/screens/Transactions/transactionHistory';
+import TransactionDetailsScreen from './src/screens/Transactions/transactionsDetails';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,7 +20,9 @@ const App = () => {
           headerShown: false,
         }}>
         <Stack.Screen name="Login" component={LoginScreen} />
-        {/* <Stack.Screen name="Dashboard" component={Dashboard} /> */}
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+        <Stack.Screen name="TransactionHistory" component={TransactionHistory} />
+        <Stack.Screen name="TransactionDetails" component={TransactionDetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
