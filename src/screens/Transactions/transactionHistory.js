764340e7324786ef986d3317fcaa4fde954f5d10
shivasakteballan <91557200+shivasakteballan@users.noreply.github.com>
2023-12-04 20:11:57 +0800

    Updated

--- src/screens/Transactions/transactionHistory.js
+++ src/screens/Transactions/transactionHistory.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import {Text, View, FlatList, StyleSheet, TouchableOpacity, RefreshControl} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, FlatList, StyleSheet, TouchableOpacity, RefreshControl, ToastAndroid} from 'react-native';
+import axios from 'axios';
 
 import {maskSensitiveInfo} from '../../utilities/utilities';
 import {transactionData} from '../../assets/constants/dummyJson';
 
 const TransactionHistory = ({navigation, route}) => {
+  const [listData, setListData] = useState(transactionData);
+  const [refreshing, setRefreshing] = useState(false);
   const biometricType = route?.params?.biometricType || {};
+
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
+    if (Object.keys(listData).length < 4) {
+      try {
+        const response = await axios.get('https://run.mocky.io/v3/38e1c35c-d128-4332-8f71-b6a01497f71f');
+        const mergedObject = {};
+
+        for (const key in transactionData) {
+          if (Array.isArray(transactionData[key]) && Array.isArray(response?.data[key])) {
+            mergedObject[key] = transactionData[key].concat(response?.data[key]);
+          } else {
+            mergedObject[key] = transactionData[key];
+          }
+        }
+
+        for (const key in response?.data) {
+          if (!transactionData.hasOwnProperty(key)) {
+            mergedObject[key] = response?.data[key];
+          }
+        }
+        setRefreshing(false)
+        setListData(mergedObject);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    else{
+      ToastAndroid.show('No more new data available', ToastAndroid.SHORT);
+      setRefreshing(false)
+    }
+  }, [refreshing]);
+
   const handleTransactionPress = transaction => {
     navigation.navigate('TransactionDetails', {transaction, biometricType});
   };
@@ -28,9 +64,9 @@ const TransactionHistory = ({navigation, route}) => {
     <View style={styles.container}>
       <Text style={styles.header}>Transaction History</Text>
       <FlatList
-        data={Object.keys(transactionData).map(date => ({
+        data={Object.keys(listData).map(date => ({
           date,
-          transactions: transactionData[date],
+          transactions: listData[date],
         }))}
         renderItem={renderTransactionGroup}
         keyExtractor={item => item.date}
